0cf7d688f41d0ba721256a8a9805fd48dbb58719
dougglez <maxfield.douglas@gmail.com>
2018-03-29 12:13:52 -0600

    did some more stuff

--- src/App.js
+++ src/App.js
@@ -6,24 +6,104 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      response: 'Nothing yet'
+      test: 'Nothing yet',
+      clicked: 'This button has not been clicked yet',
+      input: '',
+      update: 'Empty',
+      cards: []
     }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
   componentDidMount() {
+    // create a test endpoint and hit it when the page loads
+    // set the response to the 'test' prop on state
     axios.get('/api/test')
-    .then(res => {
-      this.setState({response: res.data});
-    });
+      .then(res => {
+        this.setState({test: res.data});
+      });
+    // hit the get cards endpoint so that our cards show up
+    // set the response to the cards prop on state
+    axios.get('/api/cards')
+      .then(res => {
+        this.setState({cards: res.data});
+      }); 
+    
+    // hit the update endpoint sio that our update text shows up
+    // set the response to the update prop on state
+    
   }
-  
+
+  //create a function called handle click to hit an endpoint when we click the button
+  // set the response to update the clicked prop on state
+  handleClick() {
+    console.log('clicked the button!');
+    axios.get('/api/clicked')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          clicked: res.data
+        })
+    })
+    .catch(err => console.log(err));
+
+  }
+  //POST
+// create a function to hit an endpoint whenever someone types into the input box
+// set the response to update the input prop on state
+handleChange() {
+
+}
+
+// PUT
+//create a function to hit an endpoint whenever someone types into the update endpoint
+// set the response to update the update prop on state
+handleUpdate() {
+
+}
+
+// DELETE
+// create an input to handle clicking on a card to remove it
+// update the cards array with the response from state
+handleDelete(index) {
+
+}
   render() {
+    let displayCards = this.state.cards.map((card, i) => {
+      console.log(card);
+      return <div className="card" onClick={() => this.handleDelete(i)} key={i}>{card}</div>
+    })
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {this.state.response}
+        <section>
+          <div className="response">
+            {this.state.test}
+          </div>
+          <div className="button-container">
+            <button onClick={this.handleClick}>Click Me</button>
+            <div>{this.state.clicked}</div>
+          </div>
+        </section>
+
+        <section>
+          You can type stuff here: <input type="text" onChange={this.handleChange} />
+          {this.state.input}
+        </section>
+        <section>
+          You can change this text: {this.state.update}<br/>
+          By Typing here: <input type="text" onChange={this.handleUpdate} />
+          
+        </section>
+        <section className="cards">
+          {displayCards}
+        </section>
+
       </div>
     );
   }
