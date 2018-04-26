ab43fcae1213b4e0a592e404660bf549ccb347b6
dougglez <maxfield.douglas@gmail.com>
2018-04-25 21:00:43 -0600

    more styling. is it too much? maybe

--- server/index.js
+++ server/index.js
@@ -18,41 +18,59 @@ let cards = Array(25).fill(0).map((v,i) => i+1);
 
 // Step 1-server endpoint: 
 // create a test endpoint that sends simple text
-
+app.get('/api/test', function(req, res) {
+  res.status(200).send('The test works!!!!! 😄 🌵');
+})
 
 
 // Step 2-server endpoint: 
 // create a get endpoint that sends the cards array
-
+app.get('/api/cards', function(req, res) {
+  res.status(200).send(cards);
+})
 
 
 // Step 3-server endpoint:
 // create a get endpoint that sends the textYouCanUpdate variable
-
+app.get('/api/update', function(req, res) {
+  res.status(200).send(textYouCanUpdate);
+})
 
 
 // Step 4-server endpoint:
 // create a get endpoint that sends the string "This button has been clicked"
-
+app.get('/api/clicked', function(req, res) {
+  res.status(200).send("This button has been clicked");
+})
 
 
 // Step 5-server endpoint:
 // create a post endpoint that receives a string and sends it back
 // after adding 👏 on the end
 // (copy that emoji to make it easier)
-
+app.post('/api/input', function(req, res) {
+  let { input } = req.body;
+  res.status(200).send(input);
+})
 
 
 // Step 6-server endpoint:
 // create a put endpoint that receives a new string on req.body
 // and updates the variable "textYouCanUpdate" found on line 7
 // then sends the new text back
-
+app.put('/api/update', function(req, res) {
+  textYouCanUpdate = req.body.text
+  res.status(200).send(textYouCanUpdate);
+})
 
 // Step 7-server endpoint:
 // create a delete endpoint that receives an index and removes that item from the 
 // cards array (hint: use a url param like '/:id')
-
+app.delete('/api/delete/:id', function(req, res) {
+  let { id } = req.params;
+  cards.splice(id, 1);
+  res.status(200).send(cards);
+})
 
 
 app.listen(3001, console.log('Docked at 3001'));
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     this.state = {
       test: 'Nothing yet',
       clicked: 'This button has not been clicked yet',
-      input: '',
+      input: 'aklsjfalkwjnfaskj.dfn',
       update: 'Empty',
       cards: []
     }
@@ -21,21 +21,25 @@ class App extends Component {
     // Step 1-front end request
     // create anaxios request and hit the test endpoint when the page loads
     // set the response to the 'test' property on state
- 
+    axios.get('/api/test')
+      .then( ({ data }) =>  this.setState({ test: data }));
     
     
     // Step 2-front end request
     // hit the get cards endpoint so that our cards show up
     // set the response to the 'cards' property on state
-  
-
+    axios.get('/api/cards')
+      .then( ({ data }) =>  this.setState({ cards: data }));
+    
+    
     // Step 3-front end request
     // hit the get endpoint that will send us the textYouCanUpdate variable.
     // set the response to the 'update' property on state
+    axios.get('/api/update')
+      .then( ({ data }) =>  this.setState({ update: data }));
     
-
-
-    // end of componentDidMount
+    
+    // ------------------ end of componentDidMount --------------------
   } // this bracket closes the componentDidMount method. Don't delete it
 
   // Step 4-front end request
@@ -53,6 +57,7 @@ class App extends Component {
   // whenever someone types into the input box
   // set the response to update the 'input' property on state
   handleChange(e) {
+    console.log('you changed something!');
     
   }
 
@@ -62,6 +67,7 @@ class App extends Component {
   //create a function to hit an endpoint whenever someone types into the update endpoint
   // set the response to update the update prop on state
   handleUpdate() {
+    console.log('handling the update');
 
   }
 
@@ -71,6 +77,7 @@ class App extends Component {
   // create an input to handle clicking on a card to remove it
   // update the cards array with the response from state
   handleDelete(index) {
+    console.log('handling the delete');
 
   }
 
@@ -84,37 +91,50 @@ class App extends Component {
 // but only change things at your own risk
   render() {
     let displayCards = this.state.cards.map((card, i) => {
-      return <div className="card" onClick={() => this.handleDelete(i)} key={i}>{card}</div>
+      return <div className="card" onClick={() => this.handleDelete(i)} key={i}><h2 className="card-text">{card}</h2></div>
     })
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Endpoints and Axios!</h1>
         </header>
-        <section>
-          <div className="response">
-            {this.state.test}
-          </div>
-          <div className="button-container">
-            <button onClick={this.handleClick}>Click Me</button>
-            <div>{this.state.clicked}</div>
-          </div>
-        </section>
+        <section className="body-wrapper">
+          <section>
+            <div className="response">
+              <h3 className="response_text">Test endpoint: {this.state.test}</h3>
+            </div>
+          </section>
+          <section>            
+            <div className="button-container">
+              <button className="button" onClick={this.handleClick}>Click Me</button>
+              <div className="button-text">{this.state.clicked}</div>
+            </div>
+          </section>
 
-        <section>
-          You can type stuff here: <input type="text" onChange={this.handleChange} />
-          {this.state.input}
-        </section>
-        <section>
-          You can change this text: {this.state.update}<br/>
-          By Typing here: <input type="text" onChange={this.handleUpdate} />
+          <section className="postInput-wrapper" >
+            <div className="postInput">
+              <label for="#postInput" className="postInput-label">You can type stuff here: </label>            
+              <input className="input postInput-input" id="postInput" type="text" onChange={this.handleChange} />
+            </div>
+            <div className="postInput-text">
+              And see it here: <span className="postInput-text_response">{this.state.input}</span>
+            </div>
+          </section>
+          <section className="update-wrapper">
+            <div>{this.state.update}</div>
+            <div>
+              <label>By Typing here: </label>
+              <input className="input" type="text" onChange={this.handleUpdate} />
+            </div>
+            
+          </section>
           
+          <section className="cards">
+            {displayCards}
+          </section>
         </section>
         
-        <section className="cards">
-          {displayCards}
-        </section>
 
       </div>
     );
